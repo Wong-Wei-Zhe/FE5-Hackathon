0e725917a9973f5029389246eda7d996fe583b30
Wong Wei Zhe Besquare <wongwz2828@gmail.com>
2021-10-22 02:42:43 +0800

    Completed DB Integration, Watch List Page, Changed tab content text

---
+++ anime-management/src/components/WatchList/indexptoh.js
@@ -0,0 +1,91 @@
+import React from "react";
+import Cards from "../Cards";
+import "./homepage.css";
+import {
+  collection,
+  query,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+  where,
+  getDocs,
+  update,
+  addDoc,
+} from "firebase/firestore";
+import { db } from "../../App";
+import { getAuth } from "firebase/auth";
+import { async } from "@firebase/util";
+
+const WatchListOnHold = () => {
+  const authentication = getAuth();
+  let user = authentication.currentUser;
+  const [listWatchList, setListWatchList] = React.useState([]);
+
+  React.useEffect(() => {
+    GetWatchList();
+  }, []);
+
+  const GetWatchList = async () => {
+    //console.log(user.uid);
+    const queryResult = await query(
+      collection(db, "Users", user.uid, "Anime"),
+      where("watch_status", "==", "On Hold")
+    );
+
+    let queryList = [];
+    await getDocs(queryResult).then((querySnapshot) =>
+      querySnapshot.forEach((docu) => {
+        let queryData = docu.data();
+        //console.log(queryData);
+        let structData = {
+          malId: queryData.mal_id,
+          animeTitle: queryData.anime_title,
+          animeImg: queryData.anime_img,
+          animeScore: queryData.animeScore,
+        };
+
+        console.log(structData);
+        queryList.push(structData);
+        //setListWatchList([...listWatchList, structData]);
+      })
+    );
+
+    setListWatchList(queryList);
+  };
+
+  React.useEffect(() => {
+    console.log("use effect check");
+    console.log(listWatchList);
+  }, [listWatchList]);
+
+  const checkList = () => {
+    if (listWatchList.length <= 0) {
+      return <span style={{ color: "white" }}>Empty List</span>;
+    } else {
+      return (
+        <>
+          <div className="main_container">
+            <div className="display_container">
+              {listWatchList.map((element, index) => {
+                return (
+                  <Cards
+                    key={index}
+                    ID={element.malId}
+                    animeTitle={element.animeTitle}
+                    imageSource={element.animeImg}
+                    scoreRating={element.animeScore}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </>
+      );
+    }
+  };
+
+  return <>{checkList()}</>;
+};
+
+export default WatchListOnHold;
