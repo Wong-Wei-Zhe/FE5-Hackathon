b3a378b8efbd8e0e10ceb934824a11d7b32ad2cf
Wong Wei Zhe Besquare <wongwz2828@gmail.com>
2021-10-22 10:27:11 +0800

    Added mobile page

--- anime-management/src/Pages/OnHold/index.js
+++ anime-management/src/Pages/OnHold/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import WatchListPlanToWatch from "../../components/WatchList/indexptw";
 
 const OnHold = () => {
   return (
     <Layout>
-      <div
-        style={{
-          backgroundColor: "#2d3238",
-          alignItems: "center",
-          justifyContent: "center",
-          display: "flex",
-          height: "100vh",
-        }}
-      >
-        <span
-          style={{
-            color: "white",
-          }}
-        >
-          There's nothing holding you here :P
-        </span>
-      </div>
+      <WatchListPlanToWatch></WatchListPlanToWatch>
     </Layout>
   );
 };
--- anime-management/src/Pages/Remove/index.js
+++ anime-management/src/Pages/Remove/index.js
@@ -1,26 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import WatchListComplete from "../../components/WatchList/indexcp";
 
 const Remove = () => {
   return (
     <Layout>
-      <div
-        style={{
-          backgroundColor: "#2d3238",
-          alignItems: "center",
-          justifyContent: "center",
-          display: "flex",
-          height: "100vh",
-        }}
-      >
-        <span
-          style={{
-            color: "white",
-          }}
-        >
-          You have no remove any items yet!
-        </span>
-      </div>
+      <WatchListComplete></WatchListComplete>
     </Layout>
   );
 };
