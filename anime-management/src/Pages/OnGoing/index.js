1608b1aaee868632f972eb92c8aa2192341b499a
Tay Suisin <tay@besquare.com.my>
2021-10-22 09:21:05 +0800

    Update Mobile View

--- anime-management/src/Pages/OnGoing/index.js
+++ anime-management/src/Pages/OnGoing/index.js
@@ -1,25 +1,28 @@
 import React from "react";
+import Layout from "../../components/Layout";
 
 const OnGoing = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "#2d3238",
-        textAlign: "center",
-        alignItems: "center",
-        justifyContent: "center",
-        display: "flex",
-        height: "100vh",
-      }}
-    >
-      <span
+    <Layout>
+      <div
         style={{
-          color: "white",
+          backgroundColor: "#2d3238",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          display: "flex",
+          height: "100vh",
         }}
       >
-        Please select an ANIME series to have this section filled!
-      </span>
-    </div>
+        <span
+          style={{
+            color: "white",
+          }}
+        >
+          Please select an ANIME series to have this section filled!
+        </span>
+      </div>
+    </Layout>
   );
 };
 
